e038cb92828b9cb6afd006e9b146d5c18fdee0b0
ueno4866 <rmr.4866@gmail.com>
2020-08-30 14:52:46 +0900

    first commit

---
+++ my-laravel-app/tests/Feature/BookTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Book;
+use App\Http\Controllers\BookController;
+
+
+class BookTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function home()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function store()
+    {
+        $this->post('/books/store',['title' => '初めて読んだ本','body' => '最高に面白かったやっぱりよかった']);
+        $this->assertDatabaseHas('books',[
+            'title' => '初めて読んだ本',
+            'body' => '最高に面白かったやっぱりよかった'
+        ]);
+        Book::where('title','初めて読んだ本')->delete();
+    }
+
+    /**
+     * @test
+     */
+    public function register()
+    {
+        $response = $this->get('/books/register');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function show()
+    {
+        $response = $this->get('/books/show');
+
+        $response->assertStatus(200);
+    }
+}
